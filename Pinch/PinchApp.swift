3332dbb011f0859cdda3aec14d84d58a1394f499
Yannick fumukani <slashcs7@hotmail.com>
2021-12-29 03:40:45 -0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Yannick Fumukani on 12/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
