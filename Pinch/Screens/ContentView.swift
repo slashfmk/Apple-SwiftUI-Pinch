f10993fce65e04c3cd58ab7a5e42c7a57172fb5e
Yannick fumukani <slashcs7@hotmail.com>
2021-12-30 01:28:36 -0600

    Init

---
+++ Pinch/Screens/ContentView.swift
@@ -0,0 +1,93 @@
+//
+//  ContentView.swift
+//  Pinch
+//
+//  Created by Yannick Fumukani on 12/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var isAnimating: Bool = false
+    @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = CGSize(width: 0, height: 0)
+    
+    func resetImageState(){
+        withAnimation(.spring()){
+            imageOffset = .zero
+            imageScale = 1
+        }
+    }
+    
+    var body: some View {
+        
+        NavigationView {
+            
+            ZStack {
+                //: IMAGE
+                Image("magazine-front-cover")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(10)
+                    .padding()
+                    .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
+                    .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
+                
+                    .scaleEffect(imageScale)
+                    .onTapGesture(count: 2, perform: {
+                        withAnimation(.spring()){
+                            if(imageScale == 5){
+                                resetImageState()
+                            } else {
+                                imageScale = 5
+                            }
+                        }
+                        
+                    })
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)){
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded{ value in
+                                
+                                if(imageScale <= 1){
+                                    resetImageState()
+                                }
+                                
+                                
+                            }
+                    )
+                
+            } //: ZStack
+            .navigationTitle("Pinch and Zoom")
+            .navigationBarTitleDisplayMode(.inline)
+            
+        } //: NAVIGATION VIWE
+        .navigationViewStyle(.stack)
+        .onAppear(perform: {
+            withAnimation(.linear(duration: 1.0)){
+                isAnimating = true
+            }
+        })
+        
+        .onDisappear{
+            withAnimation(.linear(duration: 1.0)){
+                isAnimating = false
+            }
+            
+        }
+        
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .preferredColorScheme(.dark)
+    }
+}
